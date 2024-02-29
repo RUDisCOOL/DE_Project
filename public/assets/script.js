91c065b2e90d421fb27ee6c4a77d1331047616f3
sthoopidshrish <149883220+sthoopidshrish@users.noreply.github.com>
2024-02-29 19:09:20 +0530

    added responsiveness

--- public/assets/script.js
+++ public/assets/script.js
@@ -29,40 +29,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-//toggle sound and play/pause button feature added.
-const video = document.getElementById('video');
-const toggleMuteButton = document.getElementById('toggle-mute');
-const togglePlayButton = document.getElementById('toggle-play');
-
-toggleMuteButton.addEventListener('click', () => {
-    video.muted = !video.muted;
-    toggleMuteButton.textContent = video.muted ? 'Unmute' : 'Mute';
-});
-
-togglePlayButton.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-        togglePlayButton.textContent = 'Pause';
-    } else {
-        video.pause();
-        togglePlayButton.textContent = 'Play';
-    }
-});
-// the buttons that dissappear before the contact section and reappear when the keyfeatures is visited.
-const keyFeaturesSection = document.getElementById('key-features');
-const controls = document.getElementById('controls');
-const contactSection = document.getElementById('contact');
-
-const observer = new IntersectionObserver(entries => {
-    if (entries[0].target.id === 'key-features' && entries[0].isIntersecting) {
-        controls.style.display = 'flex';
-    } else {
-        controls.style.display = 'none';
-    }
-}, { threshold: 0.6 }); // Adjust threshold as needed
-
-observer.observe(keyFeaturesSection);
-observer.observe(contactSection);
 
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
