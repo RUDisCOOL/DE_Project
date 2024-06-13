22c6de17d7168f0e50a443f8a7853fc0391605d1
RUDisCOOL <78923892+RUDisCOOL@users.noreply.github.com>
2024-06-13 22:09:12 +0530

    UI changes

--- app.js
+++ app.js
@@ -16,24 +16,24 @@ dbconnect.create_table();
 app.use("/public", express.static("./public"));
 
 async function getTextFromImage(data) {
-  const worker = await createWorker("eng", 1, {
-    logger: (m) => console.log("PROGRESS: " + m["progress"] * 100 + "%"),
-  });
-  const {
-    data: { text },
-  } = await worker.recognize(data);
-  // console.log(text);
-  await worker.terminate();
-  return text;
+	const worker = await createWorker("eng", 1, {
+		logger: (m) => console.log("PROGRESS: " + m["progress"] * 100 + "%"),
+	});
+	const {
+		data: { text },
+	} = await worker.recognize(data);
+	// console.log(text);
+	await worker.terminate();
+	return text;
 }
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+	destination: (req, file, cb) => {
+		cb(null, "./uploads");
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
 const upload = multer({ storage: storage }).single("xyz");
@@ -43,38 +43,38 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", routers);
 
 app.post("/sendToServer", async (req, res) => {
-  let signup_Data = req.body;
-  try {
-    let conn = await dbconnect.add_data_for_signup(signup_Data);
-    console.log(conn);
-    res.redirect("login");
-  } catch (error) {
-    console.error(error);
-    res.render("signup", { error: error });
-  }
+	let signup_Data = req.body;
+	try {
+		let conn = await dbconnect.add_data_for_signup(signup_Data);
+		console.log(conn);
+		res.redirect("login");
+	} catch (error) {
+		console.error(error);
+		res.render("signup", { error: error });
+	}
 });
 
 app.post("/sendForLogin", async (req, res) => {
-  let login_Data = req.body;
-  try {
-    let conn = await dbconnect.add_data_for_login(login_Data);
-    console.log(conn);
-    res.redirect("/");
-  } catch (error) {
-    console.error(error);
-    res.render("login", { error: error });
-  }
+	let login_Data = req.body;
+	try {
+		let conn = await dbconnect.add_data_for_login(login_Data);
+		console.log(conn);
+		res.redirect("/");
+	} catch (error) {
+		console.error(error);
+		res.render("login", { error: error });
+	}
 });
 
 app.post("/upload", (req, res) => {
-  upload(req, res, (err) => {
-    fs.readFile(`./uploads/${req.file.originalname}`, async (err, data) => {
-      if (err) return console.log("This is your error: ", err);
-      const text = await getTextFromImage(data);
-      res.render("index", { data: text });
-      // res.send(text);
-    });
-  });
+	upload(req, res, (err) => {
+		fs.readFile(`./uploads/${req.file.originalname}`, async (err, data) => {
+			if (err) return console.log("This is your error: ", err);
+			const text = await getTextFromImage(data);
+			res.render("index", { data: text });
+			// res.send(text);
+		});
+	});
 });
 //Start the server
 const PORT = 5500 || process.env.PORT;
