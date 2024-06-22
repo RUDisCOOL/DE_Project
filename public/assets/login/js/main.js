63b439fe0b77e14d5b1eb44df7d17d6ca087f88e
RUDisCOOL <78923892+RUDisCOOL@users.noreply.github.com>
2024-06-22 19:01:33 +0530

    Login Page changes here!

--- public/assets/login/js/main.js
+++ public/assets/login/js/main.js
@@ -1,6 +1,16 @@
 const loginForm = document.querySelector('#login-form');
 const signupForm = document.querySelector('#signup-form');
 
+// Toast
+const toast = document.querySelector('.toast');
+const toastIcon = document.querySelector('.toast-icon');
+const close = document.querySelector('.toast-close');
+const progress = document.querySelector('.progress');
+const text1 = document.querySelector('.text-1');
+const text2 = document.querySelector('.text-2');
+// const progressBar = document.querySelector(".progress::before");
+// End Toast
+
 let wrapper = document.querySelector('.wrapper'),
 	signUpLink = document.querySelector('.link .signup-link'),
 	signInLink = document.querySelector('.link .signin-link');
@@ -29,13 +39,49 @@ loginForm.onsubmit = async (e) => {
 		body: data,
 	});
 	const result = await response.json();
+
+	// alert('Login successful');
 	if (result.success) {
-		alert('Login successful');
+		toastIcon.style.backgroundColor = `#0e9700`;
+		toast.style.borderLeft = `8px solid #0e9700`;
+		progress.style.setProperty('--progress-before-bg', '#0e9700');
+		toastIcon.classList.remove('fa-exclamation-triangle');
+		toastIcon.classList.add('fa-check');
+		text1.innerHTML = `Success`;
+		text2.innerHTML = `You have logged in successfully!`;
+
+		toast.classList.add('active');
+		progress.classList.add('active');
+
+		setTimeout(() => {
+			toast.classList.remove('active');
+		}, 3000);
+
+		setTimeout(() => {
+			progress.classList.remove('active');
+			window.location.href = '/';
+		}, 3300);
 	} else {
-		alert('Failed ' + result.error);
+		toastIcon.style.backgroundColor = `#ff3333`;
+		toast.style.borderLeft = `8px solid #ff3333`;
+		progress.style.setProperty('--progress-before-bg', '#ff3333');
+		toastIcon.classList.remove('fa-check');
+		toastIcon.classList.add('fa-exclamation-triangle');
+		text1.innerHTML = `Error`;
+		text2.innerHTML = `${result.error}`;
+
+		toast.classList.add('active');
+		progress.classList.add('active');
+
+		setTimeout(() => {
+			toast.classList.remove('active');
+		}, 3000);
+
+		setTimeout(() => {
+			progress.classList.remove('active');
+		}, 3300);
 	}
 };
-
 signupForm.onsubmit = async (e) => {
 	e.preventDefault();
 
@@ -51,8 +97,51 @@ signupForm.onsubmit = async (e) => {
 	});
 	const result = await response.json();
 	if (result.success) {
-		alert('Sign Up successful');
+		toastIcon.style.backgroundColor = `#0e9700`;
+		toast.style.borderLeft = `8px solid #0e9700`;
+		progress.style.setProperty('--progress-before-bg', '#0e9700');
+		toastIcon.classList.remove('fa-exclamation-triangle');
+		toastIcon.classList.add('fa-check');
+		text1.innerHTML = `Success`;
+		text2.innerHTML = `You have logged in successfully!`;
+
+		toast.classList.add('active');
+		progress.classList.add('active');
+
+		setTimeout(() => {
+			toast.classList.remove('active');
+		}, 3000);
+
+		setTimeout(() => {
+			progress.classList.remove('active');
+			window.location.href = '/';
+		}, 3300);
 	} else {
-		alert('Failed ' + result.error);
+		toastIcon.style.backgroundColor = `#ff3333`;
+		toast.style.borderLeft = `8px solid #ff3333`;
+		progress.style.setProperty('--progress-before-bg', '#ff3333');
+		toastIcon.classList.remove('fa-check');
+		toastIcon.classList.add('fa-exclamation-triangle');
+		text1.innerHTML = `Error`;
+		text2.innerHTML = `${result.error}`;
+
+		toast.classList.add('active');
+		progress.classList.add('active');
+
+		setTimeout(() => {
+			toast.classList.remove('active');
+		}, 3000);
+
+		setTimeout(() => {
+			progress.classList.remove('active');
+		}, 3300);
 	}
 };
+
+close.addEventListener('click', () => {
+	toast.classList.remove('active');
+
+	setTimeout(() => {
+		progress.classList.remove('active');
+	}, 300);
+});
